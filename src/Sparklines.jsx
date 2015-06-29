3da6e08d7e768401f2069b5f8e013a32f827b7ab
Boris Yankov <borisyankov@gmail.com>
2015-06-29 06:46:55 +0300

    Add bars as an option for rendering

--- src/Sparklines.jsx
+++ src/Sparklines.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
 
+class SparklinesLine extends React.Component {
+    render() {
+
+        let points = this.props.points;
+        let svgPath = 'M' + points[0].x + ' ' + points[0].y +
+            points.map((p, i) => ' L ' + points[i].x + ' ' + points[i].y) +
+            ' L' + points[points.length - 1].x + ' 10000 L0 10000 L0 ' + points[0].y;
+
+        return (
+            <path d={svgPath}
+                stroke={this.props.stroke}
+                strokeWidth={this.props.strokeWidth}
+                fill={this.props.fill}
+                fillOpacity='0.1' />
+        )
+    }
+}
+SparklinesLine.propTypes = {
+    points: React.PropTypes.array
+};
+
+class SparklinesBars extends React.Component {
+    render() {
+
+        let points = this.props.points;
+
+        return (
+            <g>
+                {points.map((p, i) =>
+                    <rect x={p.x - 10} y={p.y} width="20" height={50 - p.y}
+                        stroke={this.props.stroke}
+                        strokeWidth={this.props.strokeWidth}
+                        fill={this.props.fill}
+                        fillOpacity='0.1' />
+                )}
+            </g>
+        )
+    }
+}
+SparklinesBars.propTypes = {
+    points: React.PropTypes.array
+};
+
+
 class Sparklines extends React.Component {
 
     constructor (props) {
@@ -33,31 +77,31 @@ class Sparklines extends React.Component {
             }
         });
 
-        let svgPath = 'M' + points[0].x + ' ' + points[0].y +
-            points.map((p, i) => ' L ' + points[i].x + ' ' + points[i].y) +
-            ' L' + points[points.length - 1].x + ' 10000 L0 10000 L0 ' + points[0].y;
-
         return (
-            <svg>
-                <path d={svgPath}
-                    stroke={this.props.lineColor}
-                    strokeWidth={this.props.lineWidth}
-                    fill={this.props.fill}
-                    fillOpacity='0.1' />
+            <svg preserveAspectRatio="xMinYMin meet">
+                {this.props.bars
+                    ? <SparklinesBars points={points}
+                        stroke={this.props.lineColor}
+                        strokeWidth={this.props.lineWidth}
+                        fill={this.props.fill} />
+                    : <SparklinesLine points={points}
+                        stroke={this.props.lineColor}
+                        strokeWidth={this.props.lineWidth}
+                        fill={this.props.fill} />
+                }
             </svg>
         );
     }
 }
-
 Sparklines.propTypes = {
     data: React.PropTypes.array,
     limit: React.PropTypes.number,
+    bars: false,
     lineColor: React.PropTypes.string,
     lineWidth: React.PropTypes.number,
     fill: React.PropTypes.string
 };
 Sparklines.defaultProps = {
-    data: [],
     limit: 100,
     lineColor: 'black',
     lineWidth: 1,
