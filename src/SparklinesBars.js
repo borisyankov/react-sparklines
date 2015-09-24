1b8031fdd68cf09e2524fcf33fe96987340d18bf
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-09-24 13:13:56 +0200

    Removed unused variables and added propTypes. Better code style.

--- src/SparklinesBars.js
+++ src/SparklinesBars.js
@@ -3,6 +3,8 @@ import React from 'react';
 export default class SparklinesBars extends React.Component {
 
     static propTypes = {
+        points: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+        height: React.PropTypes.number.isRequired,
         style: React.PropTypes.object
     };
 
@@ -12,7 +14,7 @@ export default class SparklinesBars extends React.Component {
 
     render() {
 
-        const { points, width, height, margin, style } = this.props;
+        const { points, height, style } = this.props;
         const barWidth = points.length >= 2 ? Math.max(0, points[1].x - points[0].x) : 0;
 
         return (
@@ -20,8 +22,10 @@ export default class SparklinesBars extends React.Component {
                 {points.map((p, i) =>
                     <rect
                         key={i}
-                        x={p.x} y={p.y}
-                        width={barWidth} height={Math.max(0, height - p.y)}
+                        x={p.x}
+                        y={p.y}
+                        width={barWidth}
+                        height={Math.max(0, height - p.y)}
                         style={style} />
                 )}
             </g>
