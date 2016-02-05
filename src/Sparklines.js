fb991af03eea134d4f44c1b9b639156fa71f484c
Josip Janžić <josip.janzic@gmail.com>
2016-02-05 10:58:14 +0100

    added viewBox svg prop to enable scaling with css

--- src/Sparklines.js
+++ src/Sparklines.js
@@ -45,7 +45,7 @@ class Sparklines extends React.Component {
         const points = DataProcessor.dataToPoints(data, limit, width, height, margin, max, min);
 
         return (
-            <svg width={width} height={height} style={style}>
+            <svg width={width} height={height} style={style} viewBox={`0 0 ${width} ${height}`}>
                 {
                     React.Children.map(this.props.children, function(child) {
                         return React.cloneElement(child, { points, width, height, margin });
