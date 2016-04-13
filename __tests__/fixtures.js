2e8ec9cd58c7cf77e8af21747c7d8f7b425a9054
Moti Zilberman <motiz88@gmail.com>
2016-04-13 16:34:42 +0300

    Add an auto-generated, passing, fixtures.js

--- __tests__/fixtures.js
+++ __tests__/fixtures.js
@@ -5,6 +5,6 @@ import {Sparklines, SparklinesLine} from '../src/Sparklines';
 
 export default {
 // AUTO-GENERATED PART STARTS HERE
-	'SimpleLine': {jsx: (<Sparklines data={[1, 2, 3]}><SparklinesLine /></Sparklines>), svg: ''}
+	"SimpleLine": {jsx: (<Sparklines data={[1, 2, 3]} height={60} margin={2} width={240}><SparklinesLine style={{}} /></Sparklines>), svg: "<svg width=\"240\" height=\"60\" viewbox=\"0 0 240 60\"><g><polyline points=\"2 58 120 30 238 2 238 58 2 58 2 58\" style=\"stroke:none;stroke-width:0;fill-opacity:.1;fill:slategray;\"/><polyline points=\"2 58 120 30 238 2\" style=\"stroke:slategray;stroke-width:1;stroke-linejoin:round;stroke-linecap:round;fill:none;\"/></g></svg>"},
 // AUTO-GENERATED PART ENDS HERE
 };
