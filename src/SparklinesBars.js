15638dee9c195ed48725085ab16cba01dc1fd56c
Ashwinikumar Patil <kafka-tamura@users.noreply.github.com>
2017-01-27 02:39:46 +0530

    Included change left out in previous pull request
    margin was not extracted in this.props in line 20. Added the margin variable in the deconstructing assignment.

--- src/SparklinesBars.js
+++ src/SparklinesBars.js
@@ -17,7 +17,7 @@ export default class SparklinesBars extends React.Component {
 
     render() {
 
-        const { points, height, style, barWidth, onMouseMove } = this.props;
+        const { points, height, style, barWidth, margin, onMouseMove } = this.props;
         const strokeWidth = 1 * ((style && style.strokeWidth) || 0);
         const marginWidth = margin ? 2*margin : 0;
         const width = barWidth || (points && points.length >= 2 ? Math.max(0, points[1].x - points[0].x - strokeWidth - marginWidth) : 0);
