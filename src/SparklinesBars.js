6096fe7e6ae6b3fe64e649256f976504a382d7c5
Boris Yankov <borisyankov@gmail.com>
2017-07-27 15:46:34 +0300

    Updgrade dependencies. Fix bugs

--- src/SparklinesBars.js
+++ src/SparklinesBars.js
@@ -2,40 +2,43 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class SparklinesBars extends React.Component {
+  static propTypes = {
+    points: PropTypes.arrayOf(PropTypes.object),
+    height: PropTypes.number,
+    style: PropTypes.object,
+    barWidth: PropTypes.number,
+    margin: PropTypes.number,
+    onMouseMove: PropTypes.func,
+  };
 
-    static propTypes = {
-        points: PropTypes.arrayOf(PropTypes.object),
-        height: PropTypes.number,
-        style: PropTypes.object,
-        barWidth: PropTypes.number,
-        margin: PropTypes.number,
-        onMouseMove: PropTypes.func
-    };
+  static defaultProps = {
+    style: { fill: 'slategray' },
+  };
 
-    static defaultProps = {
-        style: { fill: 'slategray' }
-    };
+  render() {
+    const { points, height, style, barWidth, margin, onMouseMove } = this.props;
+    const strokeWidth = 1 * ((style && style.strokeWidth) || 0);
+    const marginWidth = margin ? 2 * margin : 0;
+    const width =
+      barWidth ||
+      (points && points.length >= 2
+        ? Math.max(0, points[1].x - points[0].x - strokeWidth - marginWidth)
+        : 0);
 
-    render() {
-
-        const { points, height, style, barWidth, margin, onMouseMove } = this.props;
-        const strokeWidth = 1 * ((style && style.strokeWidth) || 0);
-        const marginWidth = margin ? 2*margin : 0;
-        const width = barWidth || (points && points.length >= 2 ? Math.max(0, points[1].x - points[0].x - strokeWidth - marginWidth) : 0);
-
-        return (
-            <g transform = "scale(1,-1)">
-                {points.map((p, i) =>
-                    <rect
-                        key={i}
-                        x={p.x - (width + strokeWidth)/2}
-                        y={-height}
-                        width={width}
-                        height={Math.max(0, height-p.y)}
-                        style={style}
-                        onMouseMove={onMouseMove.bind(this, p)}/>
-                )}
-            </g>
-        )
-    }
+    return (
+      <g transform="scale(1,-1)">
+        {points.map((p, i) =>
+          <rect
+            key={i}
+            x={p.x - (width + strokeWidth) / 2}
+            y={-height}
+            width={width}
+            height={Math.max(0, height - p.y)}
+            style={style}
+            onMouseMove={onMouseMove && onMouseMove.bind(this, p)}
+          />,
+        )}
+      </g>
+    );
+  }
 }
--- src/SparklinesLine.js
+++ src/SparklinesLine.js
@@ -2,63 +2,67 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class SparklinesLine extends React.Component {
+  static propTypes = {
+    color: PropTypes.string,
+    style: PropTypes.object,
+  };
 
-    static propTypes = {
-        color: PropTypes.string,
-        style: PropTypes.object
+  static defaultProps = {
+    style: {},
+    onMouseMove: () => {},
+  };
+
+  render() {
+    const { data, points, width, height, margin, color, style, onMouseMove } = this.props;
+
+    const linePoints = points.map(p => [p.x, p.y]).reduce((a, b) => a.concat(b));
+
+    const closePolyPoints = [
+      points[points.length - 1].x,
+      height - margin,
+      margin,
+      height - margin,
+      margin,
+      points[0].y,
+    ];
+
+    const fillPoints = linePoints.concat(closePolyPoints);
+
+    const lineStyle = {
+      stroke: color || style.stroke || 'slategray',
+      strokeWidth: style.strokeWidth || '1',
+      strokeLinejoin: style.strokeLinejoin || 'round',
+      strokeLinecap: style.strokeLinecap || 'round',
+      fill: 'none',
+    };
+    const fillStyle = {
+      stroke: style.stroke || 'none',
+      strokeWidth: '0',
+      fillOpacity: style.fillOpacity || '.1',
+      fill: style.fill || color || 'slategray',
+      pointerEvents: 'auto',
     };
 
-    static defaultProps = {
-        style: {}
-    };
+    const tooltips = points.map((p, i) => {
+      return (
+        <circle
+          key={i}
+          cx={p.x}
+          cy={p.y}
+          r={2}
+          style={fillStyle}
+          onMouseEnter={e => onMouseMove('enter', data[i], p)}
+          onClick={e => onMouseMove('click', data[i], p)}
+        />
+      );
+    });
 
-    render() {
-        const { data, points, width, height, margin, color, style, onMouseMove } = this.props;
-
-        const linePoints = points
-            .map((p) => [p.x, p.y])
-            .reduce((a, b) => a.concat(b));
-
-        const closePolyPoints = [
-            points[points.length - 1].x, height - margin,
-            margin, height - margin,
-            margin, points[0].y
-        ];
-
-        const fillPoints = linePoints.concat(closePolyPoints);
-
-        const lineStyle = {
-            stroke: color || style.stroke || 'slategray',
-            strokeWidth: style.strokeWidth || '1',
-            strokeLinejoin: style.strokeLinejoin || 'round',
-            strokeLinecap: style.strokeLinecap || 'round',
-            fill: 'none'
-        };
-        const fillStyle = {
-            stroke: style.stroke || 'none',
-            strokeWidth: '0',
-            fillOpacity: style.fillOpacity || '.1',
-            fill: style.fill || color || 'slategray',
-            pointerEvents: 'auto'
-        };
-
-        const tooltips = points.map((p, i) => {
-          return (<circle
-            cx={p.x}
-            cy={p.y}
-            r={2}
-            style={fillStyle}
-            onMouseEnter={(e) => onMouseMove('enter', data[i], p)}
-            onClick={(e) => onMouseMove('click', data[i], p)}
-          />)
-        });
-
-        return (
-            <g>
-                {tooltips}
-                <polyline points={fillPoints.join(' ')} style={fillStyle}/>
-                <polyline points={linePoints.join(' ')} style={lineStyle}/>
-            </g>
-        )
-    }
+    return (
+      <g>
+        {tooltips}
+        <polyline points={fillPoints.join(' ')} style={fillStyle} />
+        <polyline points={linePoints.join(' ')} style={lineStyle} />
+      </g>
+    );
+  }
 }
